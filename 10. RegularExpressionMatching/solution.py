cb0d81f86d0566e7bd4992f8821f4cbc2239c71a
unknown <vm.dung.learning@gmail.com>
2025-02-20 06:42:11 +0700

    19/2/2025

---
+++ 10. RegularExpressionMatching/solution.py
@@ -0,0 +1,48 @@
+"""
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Example 1:
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+"""
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if j == len(p):
+                ans = i == len(s)
+            else:
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j + 1 < len(p) and p[j+1] == '*':
+                    ans = dp(i, j+2) or (first_match and dp(i+1, j))
+                else:
+                    ans = first_match and dp(i+1, j+1)
+
+            memo[(i, j)] = ans
+            return ans
+
+        return dp(0, 0)
--- 8. StrToInt/solution.py
+++ 8. StrToInt/solution.py
@@ -66,3 +66,31 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        i = 0
+        n = len(s)
+        while i < n and s[i] == ' ':
+            i += 1
+
+        if i == n:
+            return 0
+
+        sign = 1
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        result = 0
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+
+            result = result * 10 + digit
+            i += 1
+        result *= sign
+        return result
